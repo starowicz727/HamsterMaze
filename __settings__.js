7b1f15a72c650b0d28581edfe5461bb1f16df84d
starowicz727 <starowicz727@gmail.com>
2022-09-11 14:52:53 +0200

    hamster maze game

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1415938.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 82199944, 87697233, 82199950, 82199953, 82199956, 82213926, 83906176, 84778893, 84786766, 84795992, 84798306, 84806100, 84823100, 84845490, 84845587, 84858712, 84858717, 84866732, 84866740, 84869040, 85572406, 86530282, 87684308, 88052100, 88069198, 88870381 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/82199941/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/82199942/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/82199940/1/ammo.js', 'preload' : true},
+];
